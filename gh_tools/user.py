a8f71a2a3797f282cf77ccdb49cfe6745abef3cc
kurages <kuragebots@gmail.com>
2022-04-14 12:37:38 +0900

    profileちょっといじった

--- gh_tools/user.py
+++ gh_tools/user.py
@@ -10,12 +10,19 @@ class User:
 		profile = self.subparsers.add_parser("profile", help=f"{self.cmd} profile -h")
 		profile.set_defaults(subcommand_func=self.profile)
 
+	def _show(self, a, b):
+		print(f"{a}{' '*(10-len(a))}{b}")
+
 	def profile(self):
 		res = self._request(get, Config.USER)
 		if res.status_code == 200:
 			data = res.json()
-			print(f"username\t{data['name']}")
-			print(f"userid\t{data['login']}")
+			self._show("username", data['name'])
+			self._show("userid", data['login'])
+			self._show("email", data['email'])
+			self._show("company", data['company'])
+			self._show("location", data['location'])
+			self._show("plan", data['plan']['name'])
 		#else:
 			print(res.json())
 		return (res.status_code, res)
